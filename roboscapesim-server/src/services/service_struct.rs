411b461fb212f94040df0d6eaeb07448095d14bc
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2023-10-27 17:57:56 -0500

    Announce less often

--- roboscapesim-server/src/services/service_struct.rs
+++ roboscapesim-server/src/services/service_struct.rs
@@ -73,4 +73,4 @@ pub(crate) fn setup_service(definition: ServiceDefinition, service_type: Service
     service
 }
 
-pub const DEFAULT_ANNOUNCE_PERIOD: Duration = Duration::from_secs(45);
+pub const DEFAULT_ANNOUNCE_PERIOD: Duration = Duration::from_secs(360);
