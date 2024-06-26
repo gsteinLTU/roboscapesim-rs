8134c204e9075dabfd210bda560fc3bc6070a6b1
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-06-26 11:11:54 -0500

    Try to auto build caddy container and add zerossl

--- update_caddyfile.sh
+++ update_caddyfile.sh
@@ -2,3 +2,4 @@ DASH_IP="`curl -s http://checkip.amazonaws.com | cut -d " " -f 2 | tr . -`"
 
 cp Caddyfile.template Caddyfile
 sed -i -e  "s/DASH_IP/$DASH_IP/g" Caddyfile
+sed -i -e  "s/ZEROSSL_API_KEY/$ZEROSSL_API_KEY/g" Caddyfile
