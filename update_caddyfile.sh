05e756d54f069034c5149641d4f974da63e758f7
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-06-28 10:34:01 -0500

    Update caddy template to allow providing cert and key

--- update_caddyfile.sh
+++ update_caddyfile.sh
@@ -1,5 +1,21 @@
-DASH_IP="`curl -s http://checkip.amazonaws.com | cut -d " " -f 2 | tr . -`"
+#!/bin/bash
 
+# Get the current IP address in the DASH_IP format
+DASH_IP="`curl -s http://checkip.amazonaws.com | tr . -`"
+
+# Copy the template to the actual Caddyfile
 cp Caddyfile.template Caddyfile
-sed -i -e  "s/DASH_IP/$DASH_IP/g" Caddyfile
-sed -i -e  "s/ZEROSSL_API_KEY/$ZEROSSL_API_KEY/g" Caddyfile
+
+# Replace the placeholder with the actual DASH_IP
+sed -i -e "s/DASH_IP/$DASH_IP/" Caddyfile
+
+# Check if CERT and KEY environment variables are set
+if [[ -n "$CERT" && -n "$KEY" ]]; then
+  # Append the TLS configuration to the Caddyfile
+  echo "
+tls {
+    certificate \"$CERT\"
+    key \"$KEY\"
+}
+" >> Caddyfile
+fi
