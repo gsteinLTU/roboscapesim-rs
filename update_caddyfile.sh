b33ec503dc6fff089629a8d602c4d49649a06e08
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-07-01 11:17:45 -0500

    Add logging to update_caddyfile

--- update_caddyfile.sh
+++ update_caddyfile.sh
@@ -2,6 +2,7 @@ echo "Updating Caddyfile"
 
 # Get the current IP address in the DASH_IP format
 DASH_IP="`curl -s http://checkip.amazonaws.com | tr . -`"
+echo "DASH_IP: $DASH_IP"
 
 # Copy the template to the actual Caddyfile
 cp Caddyfile.template Caddyfile
@@ -11,6 +12,8 @@ sed -i -e "s/DASH_IP/$DASH_IP/" Caddyfile
 
 # Check if CERT and KEY environment variables are set
 if [[ -n "$CERT" && -n "$KEY" ]]; then
+  echo "TLS certificate and key are provided"
+
   # Decode and write the CERT and KEY to files
   echo "$CERT" | base64 -d > cert.pem
   echo "$KEY" | base64 -d > key.pem
@@ -21,10 +24,12 @@ if [[ -n "$CERT" && -n "$KEY" ]]; then
     echo "Certificate has expired"
     sed -i -e "s/EXTRA//" Caddyfile
   else
+    echo "Certificate is still valid"
     # Replace the placeholder with TLS configuration
     sed -i -e "s/EXTRA/tls cert.pem key.pem/" Caddyfile
   fi
 else
+  echo "TLS certificate and key are not provided"
   # Replace the placeholder with no TLS configuration
   sed -i -e "s/EXTRA//" Caddyfile
 fi
